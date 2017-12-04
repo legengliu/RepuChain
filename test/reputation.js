0eb2284f14275479f1ad76b87a171a6386f5a644
legengliu <legengliu@berkeley.edu>
2017-12-03 16:38:20 -0800

    add test for getting ratings as pairs of addresses

--- test/reputation.js
+++ test/reputation.js
@@ -40,22 +40,20 @@ contract('Reputation', function(accounts) {
     assert.equal(g_1.toNumber(), 4, "should have 4 total ratings.");
   })
 
-/*
-  it("...non owner cannot add rating", async function() {
-    graph = await Graph.new({from: accounts[0]});
+  it("...should return ratings as pairs of addresses", async function() {
+    rep = await Reputation.new();
+    await rep.rate(accounts[2], {from: accounts[0]});
+    await rep.rate(accounts[3], {from: accounts[1]});
+    await rep.rate(accounts[4], {from: accounts[2]});
+    await rep.rate(accounts[5], {from: accounts[3]});
 
-    await graph.addRating(accounts[1], accounts[2], {from: accounts[2]});
-    await graph.addRating(accounts[1], accounts[3], {from: accounts[2]});
-    await graph.addRating(accounts[1], accounts[4], {from: accounts[2]});
+    let g_1 = await rep.getNumRatings.call();
+    assert.equal(g_1.toNumber(), 4, "should have 4 total ratings.");
 
-    await graph.addRating(accounts[3], accounts[7], {from: accounts[0]});
-
-    let g_1 = await graph.getOutgoingRatings.call(accounts[1]);
-    assert.equal(g_1.length, 0, "should have 0 outgoing ratings.");
-
-    let g_3 = await graph.getOutgoingRatings.call(accounts[3]);
-    assert.equal(g_3.length, 1, "should have 1 outgoing rating1.");
+    for (let i = 0; i < g_1.toNumber(); i++) {
+      let r = await rep.getRating(i);
+      assert.equal(r[0], accounts[i]);
+      assert.equal(r[1], accounts[i + 2]);
+    }
   })
-*/
-
 });
